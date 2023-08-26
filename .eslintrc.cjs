1889b03762ea7b5ddbb849f23eee98ed73d855e0
wtfgn <exal59@yahoo.com>
2023-08-26 19:40:42 +0800

    setup eslint & prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked',
+        'prettier'
+    ],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname
+    },
+    root: true,
+    rules: {
+    },
+    env: {
+        browser: true,
+        amd: true,
+        node: true
+    }
+};
---
+++ prettier.config.cjs
@@ -0,0 +1,20 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+    printWidth: 100,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    jsxSingleQuote: false,
+    trailingComma: 'none',
+    bracketSpacing: true,
+    arrowParens: 'always',
+    rangeStart: 0,
+    rangeEnd: Infinity,
+    requirePragma: false,
+    insertPragma: false,
+    proseWrap: 'preserve',
+    htmlWhitespaceSensitivity: 'css',
+    endOfLine: 'lf'
+};
